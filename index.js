1b3de0934ce2da6731d5319fb45a40dd97a06b75
BorysPresn <boryspresniak@gmail.com>
2023-11-30 11:01:34 +0100

    Add some basic classes

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ class Game {
         return board;
     }
 
+//1.2. Метод startNewGame должен инициализировать пустое игровое поле и сообщать о начале новой игры.
+   
     startNewGame() {
         this.board = this.initBoard();
         console.log("New Games started, field was reseted");
     }
+
+//1.3. Метод displayBoard должен выводить текущее состояние игрового поля.
 
     displayBoard() {
         console.log("current board state");
@@ -30,8 +34,38 @@ class Game {
     }
 }
 
+
+
+// Шаг 2: Создание объекта Player
+// 2.1. Создай класс Player с конструктором. В конструкторе определи свойство, которое будет представлять знак игрока (X или O).
+
+class Player {
+    constructor(symbol) {
+        this.playerType = symbol;
+    }
+
+// 2.2. Метод makeMove должен принимать координаты (ряд и столбец) от игрока и вносить его знак на игровое поле.
+
+    makeMove(row, col) {
+        game.board[row][col] = this.playerType;
+    }
+// 2.3. Метод getMove должен запрашивать у игрока ввод координат для совершения хода.
+
+    getMove() {
+       const row = +prompt('Enter coordinates of row: ' );
+       const col = +prompt('Enter coordinates of collumn: ');
+       const coordinates = [row, col];
+       return coordinates;
+    }
+}
+
+
+
+
+// Шаг 3: Взаимодействие между объектами Game и Player
+// 3.1. Метод play в классе Game должен содержать основной цикл игры, в котором вызываются методы makeMove и getMove для каждого игрока.
+// 3.2. После каждого хода вызывай метод displayBoard для отображения текущего состояния игрового поля.
+// 3.3. Проверяй выигрышные комбинации после каждого хода и объявляй победителя или ничью.
 const game = new Game();
 game.startNewGame();
 game.displayBoard();
-//1.2. Метод startNewGame должен инициализировать пустое игровое поле и сообщать о начале новой игры.
-//1.3. Метод displayBoard должен выводить текущее состояние игрового поля.
