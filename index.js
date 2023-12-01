62e358de3d1d4f5b59d25834ed718205c3a2b788
BorysPresn <boryspresniak@gmail.com>
2023-12-01 13:16:13 +0100

    add checkWinner method

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ class Game {
    
     startNewGame() {
         this.board = this.initBoard();
+        this.winner = null;
         console.log("New Game started");
     }
 
@@ -38,22 +39,41 @@ class Game {
 // 3.2. После каждого хода вызывай метод displayBoard для отображения текущего состояния игрового поля.
 // 3.3. Проверяй выигрышные комбинации после каждого хода и объявляй победителя или ничью.
     
+    checkWinner() {
+        const board = this.board;
+
+        for(let i = 0; i < 3; i++){
+            if(board[0][i] === board[1][i] && board[1][i] === board[2][i]) { //checking vertical lines
+                return board[0][i];
+            }
+            if(board[i][0] === board[i][1] && board[i][1] === board[i][2]) { // checking horizontal lines
+                return board[i][0];
+            }
+        }
+        return null;
+    }
+
     play(){
         this.startNewGame();
-        while(winner === null) {
-            player1.getMove();
-            player1.makeMove();
+        while(this.winner === null) {
+            let move1 = player1.getMove();
+            player1.makeMove(move1);
+            this.displayBoard();
             if(this.checkWinner()){
-                console.log('Player X win!!!');
-                break;
+                this.winner = player1.playerType;
+                console.log(`Player ${this.winner} win!!!`);
+                return;
             };
-            player2.getMove();
-            player2.makeMove();
+            let move2 = player2.getMove();
+            player2.makeMove(move2);
+            this.displayBoard();
             if(this.checkWinner()){
-                console.log('Player O win!!!');
-                break;
+                this.winner = player2.playerType;
+                console.log(`Player ${this.winner} win!!!`);
+                return;
             };
         }
+
     }
 }
 
@@ -63,20 +83,21 @@ class Game {
 // 2.1. Создай класс Player с конструктором. В конструкторе определи свойство, которое будет представлять знак игрока (X или O).
 
 class Player {
-    constructor(symbol) {
+    constructor(symbol, game) {
         this.playerType = symbol;
+        this.game = game;
     }
 
 // 2.2. Метод makeMove должен принимать координаты (ряд и столбец) от игрока и вносить его знак на игровое поле.
 
     makeMove({row, col}) {
-        game.board[row][col] = this.playerType; // insert a player symbol to the game board (X or O)
+        this.game.board[row][col] = this.playerType; // insert a player symbol to the game board (X or O)
     }
 // 2.3. Метод getMove должен запрашивать у игрока ввод координат для совершения хода.
 
     getMove() {
-       const row = +prompt('Enter coordinates of row: ' );
-       const col = +prompt('Enter coordinates of collumn: ');
+       const row = parseInt(prompt('Enter coordinates of row: '));
+       const col = parseInt(prompt('Enter coordinates of column:'));
        return {row, col};
     }
 }
@@ -86,7 +107,6 @@ class Player {
 
 
 const game = new Game();
-const player1 = new Player("X");
-const player2 = new Player("O");
-game.startNewGame();
-game.displayBoard();
+const player1 = new Player("X", game);
+const player2 = new Player("O", game);
+game.play();
